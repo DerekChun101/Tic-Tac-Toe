86d4435a0333353748f1586304eb708ff93d8a02
Derek Chun <derekchun2013@gmail.com>
2024-01-02 18:43:56 -0500

    Created new factory functions

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const GameBoard = (function() {
+    const gameBoard = [];
+    return { gameBoard };
+})();
+
+const displayControl = (function() {
+    
+})();
+
+function createPlayer(name) {
+    const Username = name;
+    return { Username };
+}
+
